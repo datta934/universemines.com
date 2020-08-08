82998d9304e6ff2c93774c6478af28c167b77d4d
mauaa <mauaa@koder.com>
2020-08-08 17:04:25 +0530

    Added folder struture, assets, code, Home

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function ($) {
+  $(function () {
+    $('.slider').slider({
+      indicators: false
+    });
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
